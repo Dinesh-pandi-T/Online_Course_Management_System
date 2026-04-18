f192a59916a6fbdae4313771191ae1f02cf64bd3
Dinesh <avinashdhdh2005@gmail.com>
2026-04-18 14:42:39 +0530

    b

--- backend-springboot/src/main/java/com/onlinecourse/config/SecurityConfig.java
+++ backend-springboot/src/main/java/com/onlinecourse/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://online-course-management-system-ivgxhi8pl-shadow-strikers.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
